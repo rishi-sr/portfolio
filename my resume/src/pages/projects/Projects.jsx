7768f176bf413a8594051d49fdbcd210ee7e53fb
rishi-sr <rishi16esrivastava@gmail.com>
2025-08-23 23:53:11 +0530

    commit

--- my resume/src/pages/projects/Projects.jsx
+++ my resume/src/pages/projects/Projects.jsx
@@ -1,14 +1,39 @@
 import React, { useEffect, useState } from "react";
 import './projects.scss';
 
-const Projects = () => {
-  const [projects, setProjects] = useState([]);
+const projectsData = [
+  {
+    name: "Stone Paper Scissors",
+    url: "sps.png",
+    link: "https://sunny-duckanoo-3ba47b.netlify.app/",
+    p: "This is a Stone Paper Scissors game where you play against the computer by choosing stone, paper, or scissors. It shows both scores in real-time with a clean and interactive design.",
+    skills: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Green Gold Club E-Commerce Website",
+    url: "ggc.png",
+    link: "https://sunny-duckanoo-3ba47b.netlify.app/",
+    p: "A full-stack e-commerce platform for eco-friendly products, designed for Green Gold Club. It features product listings, cart, and secure checkout, along with responsive design for better accessibility.",
+    skills: ["Reactjs", "Expressjs", "MongoDB", "Nodejs"]
+  },
+  {
+    name: "HeritEdge",
+    url: "herit.png",
+    link: "https://sunny-duckanoo-3ba47b.netlify.app/",
+    p: "A cultural heritage web app showcasing India’s rich traditions. It includes information pages, user accounts, and interactive sections to explore festivals, arts, and culture.",
+    skills: ["Reactjs", "Expressjs", "MongoDB", "Nodejs"]
+  },
+  {
+    name: "Food Recipe Site",
+    url: "fr.png",
+    link: "https://foodrecipesweb.netlify.app/",
+    p: "A recipe website where users can browse and explore different cuisines. It provides ingredients, step-by-step cooking methods, and a clean responsive design.",
+    skills: ["HTML", "CSS", "JavaScript"]
+  }
+];
 
-  useEffect(() => {
-  fetch("http://localhost:5000/api/projects")
-    .then(res => res.json())
-    .then(data => setProjects(data));
-}, []);
+const Projects = () => {
+  
 
 
   return (
@@ -19,7 +44,7 @@ const Projects = () => {
         <p>A showcase of projects I have worked on, highlighting my skills and experience in various technologies</p>
       </div>
       <div className="cards">
-        {projects.map((project, i) => (
+        {projectsData.map((project, i) => (
           <div className="card" key={i}>
             <div className="img">
               <a href={project.link} target="_blank" rel="noopener noreferrer">
